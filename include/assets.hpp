80fc01a94230d0e992a75715cbf49d8bdcedb73c
Dyokiin <theo.arnauld-des-lions@univ-tlse3.fr>
2021-06-26 21:55:10 +0200

    P L A N T & D O O R V2

--- include/assets.hpp
+++ include/assets.hpp
@@ -7,18 +7,22 @@
 /* Murs et fenêtres */
 extern float wallz[15900][3];
 extern float wallz2[22877][3];
-extern float wallz3[11252][3];
+extern float wallz3[12532][3];
 extern float wallx2[34502][3];
 extern float wallx3[34502][3];
 
 /* Portes */
 extern float doorx[1952][3];
+extern float doorx2[1952][3];
 extern float hdx[16][3];
+extern float hdx2[16][3];
 
 /* Plantes */
 
 extern float pot[62][3];
 extern float dirt[23][3];
-extern float plant[8][3];
+extern float plant1[8][3];
+extern float plant2[100][3];
+extern float flower[20][3];
 
 #endif
--- src/Scene/createScene.cpp
+++ src/Scene/createScene.cpp
@@ -7,12 +7,13 @@ SceneNode* initScene(){
 
     /* On construit d'abord tous les objets */
 
-//plante
+//plantes
     ObjectNode* plante1 = new ObjectNode(makePoint(20.,20.,20.));
-    std::list<Object> lplante = {Object(pot),
-                                  Object(dirt),
-                                  Object(plant)};
+    std::list<Object> lplante = {Object(pot), Object(dirt), Object(plant1)};
     plante1->setObjects(&lplante);
+    ObjectNode* plante2 = new ObjectNode(makePoint(145.,1.,-15.));
+    lplante = {Object(pot), Object(dirt), Object(plant2), Object(flower)};
+    plante2->setObjects(&lplante);
 
 //murs
     ObjectNode* wall = new ObjectNode(makePoint(0.,0.,0.));
@@ -23,11 +24,14 @@ SceneNode* initScene(){
         ObjectNode* door = new ObjectNode(makePoint(0.,0.,-29.));
         std::list<Object> ldoor = {Object(doorx), Object(hdx)};
         door->setObjects(&ldoor);
+        ObjectNode* door2 = new ObjectNode(makePoint(130.,0.,5));
+        ldoor = {Object(doorx2), Object(hdx2)};
+        door2->setObjects(&ldoor);
 
     /* On définit les scènes secondaire et on fait les affectations */
 
 
-    std::list<ObjectNode*> l = {door,wall,plante1};
+    std::list<ObjectNode*> l = {door, door2, wall, plante1, plante2};
     scene->elements = l;
     return scene;
 }
